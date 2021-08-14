36e00a1e7de13b257bb47aba94f2057ef0d830fb
mayank-wadhwa-git <68892762+mayank-wadhwa-git@users.noreply.github.com>
2021-08-14 19:53:20 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 function submitForm(){
     $.ajax({
-        url:"https://api.apispreadsheets.com/data/16896/",
-        // url: "https://api.apispreadsheets.com/data/16890/",
+        url: "https://api.apispreadsheets.com/data/16890/",
         type:"post",
         data:$("#contactForm").serializeArray(),
         success: function(){
