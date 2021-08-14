0c2e1eb3023b944b7cf904a78a9057538465ee2b
mayank-wadhwa-git <68892762+mayank-wadhwa-git@users.noreply.github.com>
2021-08-14 19:34:49 +0530

    updated joinUs

--- js/joinUsForm.js
+++ js/joinUsForm.js
@@ -2,7 +2,7 @@ function submitJoinUsForm(){
     $.ajax({
         url: "https://api.apispreadsheets.com/data/16893/",
         type:"post",
-        data:$("#contactForm").serializeArray(),
+        data:$("#joinUsForm").serializeArray(),
         success: function(){
             window.location.replace("redirect.html");
         },
